e8bb918669f3ef6ab196e27305d4e213289c3d63
Knightmare-sama <shahbaaz23khan@gmail.com>
2021-04-08 09:07:49 +0530

    Added UnionDistinct.java

---
+++ Arrays/Rearrange.java
@@ -0,0 +1,52 @@
+package Arrays;
+
+import java.io.*;
+import java.util.*;
+
+//******************************************************************************************** */
+//Arrange all negative elements of the array to left side and all positive arrays to right side
+//******************************************************************************************** */
+
+class DriverRearrange {
+    public static void main(String args[]) throws IOException {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the Size of Array");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter the elemnts of Array");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        new Rearrange().rearrange(arr, arr.length);
+
+        sc.close();
+    }
+}
+
+class Rearrange {
+
+    public void rearrange(int arr[], int n) {
+        int j = 0, temp;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] < 0) {
+                if (i != j) {
+                    temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+                j++;
+            }
+        }
+        printArray(arr);
+    }
+
+    private void printArray(int[] arr) {
+        System.out.println("The Rearranged Array is ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
---
+++ Arrays/UnionDistinct.java
@@ -0,0 +1,41 @@
+package Arrays;
+
+// Given 2 arrays find distinct elements among them by doing some Union or some shet
+
+import java.util.*;
+
+class UnionDistinct {
+
+    static void printUnion(int[] a, int n, int[] b, int m) {
+        // create new hashmap hashmaop(keys,values)
+        // hashmap doesnt contain repetitive values, all keys are distinct
+        Map<Integer, Integer> mp = new HashMap<Integer, Integer>();
+
+        // Inserting array elements in mp
+        for (int i = 0; i < n; i++) {
+            // storing the keys so that even if they are repeated it just counts it once
+            mp.put(a[i], i);
+        }
+        for (int i = 0; i < m; i++) {
+            // storing distinct keys idk why they using i as the value here lol?!
+            mp.put(b[i], i);
+        }
+
+        System.out.println("The union set of both arrays is :");
+        for (Map.Entry mapElement : mp.entrySet()) {
+            System.out.print(mapElement.getKey() + " ");
+
+            // mp will contain only distinct
+            // elements from array a and b
+        }
+        System.out.println("The number of disctinct elements is " + mp.size());
+    }
+
+    // Driver Code
+    public static void main(String[] args) {
+        int a[] = { 1, 2, 5, 6, 2, 3, 5 };
+        int b[] = { 2, 4, 5, 6, 8, 9, 4, 6, 5 };
+
+        printUnion(a, 7, b, 9);
+    }
+}
