91d14a2d32306b46f1f785d03471677a06198202
Knightmare-sama <shahbaaz23khan@gmail.com>
2021-04-15 15:20:14 +0530

    Added Hard Gap Method

--- Arrays/MergeArray.java
+++ Arrays/MergeArray.java
@@ -13,12 +13,17 @@ import java.util.*;
 // to form 
 // arr1[] = {0, 1, 2, 3}
 // arr2[] = {5, 6, 7, 8, 9}
+
 class DriverMergeArray {
     public static void main(String[] args) {
         int arr1[] = { 1, 3, 5, 7 };
         int arr2[] = { 0, 2, 6, 8, 9 };
 
-        new MergeArray().mergeArray(arr1, arr2);
+        int n = arr1.length;
+        int m = arr2.length;
+
+        // new MergeArray().mergeArray(arr1, arr2);
+        new MergeArray().mergeArrayGap(arr1, arr2, n, m);
     }
 }
 
@@ -46,6 +51,62 @@ class MergeArray {
         Arrays.sort(arr2);
         for (int j = 0; j < arr2.length; j++) {
             System.out.print(arr2[j] + " ");
+        }
+
+    }
+
+    // This is HARDD Gap method
+    private int nextGap(int gap) {
+        if (gap <= 1) {
+            return 0;
+        }
+        return (gap / 2) + (gap % 2);
+    }
+
+    public void mergeArrayGap(int arr1[], int arr2[], int n, int m) {
+        int i = 0, j = 0, gap = n + m;
+
+        for (gap = nextGap(n + m); gap > 0; gap = nextGap(gap)) {
+
+            // comparing elements of first array
+            for (i = 0; i + gap < n; i++) {
+                if (arr1[i] > arr1[i + gap]) {
+                    int temp = arr1[i];
+                    arr1[i] = arr1[i + gap];
+                    arr1[i + gap] = temp;
+                }
+            }
+
+            // comparing elements in both arrays
+            for (j = gap > n ? gap - n : 0; i < n && j < m; i++, j++) {
+                if (arr1[i] > arr2[j]) {
+                    int temp = arr1[i];
+                    arr1[i] = arr2[j];
+                    arr2[j] = temp;
+                }
+            }
+
+            if (j < m) {
+
+                // comparing elements of second array
+                for (j = 0; j + gap < m; j++) {
+                    if (arr2[j] > arr2[j + gap]) {
+
+                        int temp = arr2[j];
+                        arr2[j] = arr2[j + gap];
+                        arr2[j + gap] = temp;
+                    }
+                }
+            }
+
+        }
+        for (int k = 0; k < n; k++) {
+
+            System.out.print(arr1[k] + " ");
+        }
+
+        for (int k = 0; k < m; k++) {
+            System.out.print(arr2[k] + " ");
         }
 
     }
